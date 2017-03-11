9c6e37836a7125767f5b48b293d4c76ece4c9120
garnan <garnanpacheco@gmail.com>
2017-03-10 19:52:18 -0600

    mata burbujas el esqueleto kinect

--- Assets/Scripts/Burbuja.cs
+++ Assets/Scripts/Burbuja.cs
@@ -2,17 +2,22 @@
 using System.Collections;
 
 public class Burbuja : MonoBehaviour {
-
+	public Color colorStart = Color.red;
+	public Color colorEnd = Color.green;
+	public float duration = 1.0F;
 	public GameObject burbujas;
+	public Renderer rnd;
 
 	// Use this for initialization
 	void Start () {
-	
+		rnd = GetComponent<Renderer> ();
+		rnd.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float lerp = Mathf.PingPong(Time.time, duration) / duration;
+		rnd.material.color = Color.Lerp(colorStart, colorEnd, lerp);
 	}
 
 	void OnCollisionEnter(Collision Col) {
---
+++ Assets/Scripts/mataburbujas.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mataburbujas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision Col) {
+		if(Col.gameObject.GetComponents<Burbuja>()!=null){
+			Debug.Log ("mataste burbujas");
+		}
+
+	}
+}
